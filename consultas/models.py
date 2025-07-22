51aac591fc88ee3491659442ee1a5077f2dff489
Paulo Mateus <paulo.fiden96@gmail.com>
2025-07-22 17:31:05 -0300

    feat: criação de endpoints e testes via PowerShell

--- consultas/models.py
+++ consultas/models.py
@@ -10,8 +10,8 @@ class Consulta(models.Model):
         ('FN', 'Finalizada'),
     ]
 
-    paciente = models.CharField(max_length=150)
-    medico = models.CharField(max_length=150)
+    paciente = models.ForeignKey('Paciente', on_delete=models.CASCADE, related_name='consultas')
+    medico = models.ForeignKey('Medico', on_delete=models.CASCADE, related_name='consultas')
     data_hora = models.DateTimeField()
     motivo = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='AG')
@@ -19,8 +19,8 @@ class Consulta(models.Model):
     atualizado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.paciente} com {self.medico} em {self.data_hora.strftime('%d/%m/%Y %H:%M')} - {self.get_status_display()}"
-    
+        return f"{self.paciente.nome} com {self.medico.nome} em {self.data_hora.strftime('%d/%m/%Y %H:%M')} - {self.get_status_display()}"
+
 
 class Paciente(models.Model):
     nome = models.CharField(max_length=100)
